e13ffd76c32aff23a141b129bd58d4a2e31c77a0
Omer Eisenberg <omereis@yahoo.com>
2024-09-03 10:39:11 +0300

    work hours info class

---
+++ WorkHoursInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkHours {
+	public class TWorkHoursInfo {
+		private int id;
+		private DateTime? m_dtStart;
+		private DateTime? m_dtEnd;
+		public TWorkHoursInfo () {
+			Clear ();
+		}
+		public void Clear () {
+		m_dtStart = null;
+		}
+	}
+}
